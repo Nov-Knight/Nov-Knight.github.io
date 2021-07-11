b562af76aa0b0573bd7f38509d89a92669258552
Nov-Knight <spmst@naver.com>
2021-07-11 14:44:37 +0900

    momentum

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = `./img/${chosenImage}`;
+
+const bgUrl = `url("${bgImage}")`;
+
+console.log(bgUrl);
+
+document.body.style.backgroundImage = bgUrl;
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "9967ee2f3a9c9bde3873639db50fc958";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} ${data.main.temp}℃`;
+      city.innerText = `${data.name}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);

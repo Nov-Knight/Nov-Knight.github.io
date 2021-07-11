5c90e5714f565e47ac3bc3a6862af154d250d1df
Nov-Knight <spmst@naver.com>
2021-07-12 00:50:00 +0900

    weather fix

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "9967ee2f3a9c9bde3873639db50fc958";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
